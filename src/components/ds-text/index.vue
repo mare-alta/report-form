053e015ee2b29c05c9bbc5c8e8126c357c3f5706
JoãoScheuermann <scheuermann.design@gmail.com>
2019-12-08 05:27:15 -0300

    FIX PORRA

--- src/components/ds-text/index.vue
+++ src/components/ds-text/index.vue
@@ -30,6 +30,42 @@ export default {
 </script>
 
 <style lang="scss">
+$layout-grid-mobile-columns: 6;
+$layout-grid-mobile-gutter: 16px;
+$layout-grid-mobile-min-width: 320px;
+$layout-grid-mobile-max-width: 740px;
+
+$layout-grid-tablet-columns: 8;
+$layout-grid-tablet-gutter: 16px;
+$layout-grid-tablet-min-width: $layout-grid-mobile-max-width;
+$layout-grid-tablet-max-width: 980px;
+
+$layout-grid-desktop-columns: 12;
+$layout-grid-desktop-gutter: 16px;
+$layout-grid-desktop-min-width: $layout-grid-tablet-max-width;
+$layout-grid-desktop-max-width: 1240px + (2 * $layout-grid-desktop-gutter);
+
+@mixin mobile () {
+  @media screen and (max-width: $layout-grid-mobile-max-width) {
+    @content
+  }
+};
+
+@mixin tablet () {
+  @media screen
+    and (min-width: $layout-grid-mobile-max-width)
+    and (max-width: $layout-grid-desktop-min-width) {
+    @content
+  }
+};
+
+@mixin desktop () {
+  @media screen and (min-width: $layout-grid-desktop-min-width) {
+    @content
+  }
+};
+
+
 .text {
   font-family: arial, sans-serif;
   font-size: 4rem;
